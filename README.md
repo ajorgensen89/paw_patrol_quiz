fc40ea4c11233c4c70629646974c3cd5320e944f
ajorgensen89 <ajorgensen89@gmail.com>
2022-12-15 16:27:17 +0000

    Add more html pages for wesbites and logos for navigation/audio links on all pages. Add 6 questions to test functions will work when functions are built.

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Paw Patrol Kids Quiz.
 
-
-
-
+The purpose of this website is to design a Quiz based website for children about the topic of `Paw Patrol`.
+<br>
+HTML, CSS and JavaScript has been used together for this project to produce a multiple questioned Quiz.
 ***
-### Table of Contents
+***
+### Table of Contents.
 * [User Experience](#user-experience)
 	- [Wireframe](#wireframe)
 * [Features](#features)
@@ -23,3 +24,14 @@
 * [Deployment](#deployment)
 * [Technologies](#technologies)
 ***
+## Technologies.
+[Favicon Icon](https://www.favicon-generator.org/)<br>
+One of the characters in Paw Patrol is Everest. I used her colours throughout the website and for the favicon icon on the website after using the generator on the picture file. 
+
+### Credits
+
+* Fonts from [Google Fonts](https://fonts.google.com/)
+* Icons from [Font Awesome](https://fontawesome.com/)
+* Wireframe from [Balsamiq](https://balsamiq.com/)
+* [Favicon Icon](https://www.favicon-generator.org/) generator wesbite
+* Favicon [image](https://www.bing.com/images/search?view=detailV2&ccid=%2BJyjEgf8&id=1C9235A2E43F8DACE3ABFA5ECA46B0C6EA59D9CF&thid=OIP.-JyjEgf8_qN5SGjxre2cngHaIB&mediaurl=https%3A%2F%2Fwww.vippng.com%2Fpng%2Fdetail%2F223-2230436_stickerbygoodcat30-everest-pawpatrol-everestpawpatrol-paw-patrol-everest.png&exph=996&expw=920&q=everest+paw+patrol&simid=608045310985327084&form=IRPRST&ck=D75FA36506DA9C1F967833C435131981&selectedindex=5&ajaxhist=0&ajaxserp=0&vt=0&sim=11&cdnurl=https%3A%2F%2Fth.bing.com%2Fth%2Fid%2FR.f89ca31207fcfea3794868f1aded9c9e%3Frik%3Dz9lZ6sawRspe%252bg%26pid%3DImgRaw%26r%3D0) source
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,100 @@
-console.log("working");
+document.addEventListener("DOMContentLoaded", function () {
+    let level = document.getElementsByTagName("button");
+
+    for (let button of level) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                findOutAnswer();
+            } else {
+                let gamelevel = this.getAttribute("data-type");
+                quizGame(gameLevel);
+            }
+        });
+    }
+    // so enter can be pressed to submit answer instead of moving mouse and clicking.
+    document.getElementById("my-answer").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            findOutAnswer();
+        }
+    });
+});
+document.getElementsById("audio"); {
+    
+}
+
+
+
+let userName = [""];
+
+let numberOfQuestions = [
+    {
+        question: "What is naughty Majors name in Paw Patrol who cases problems for the Paw Patrol gang?",
+        answers: {
+            a: "Major Goodway",
+            b: "Major Humdinger",
+            c: "Major Johnson"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What name is given to the puppy who wears a blue police unifrom in Paw Patrol?",
+        answers: {
+            a: "Chase",
+            b: "Rumble",
+            c: "Tracker"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the name of Major Goodways pet chicken?",
+        answers: {
+            a: "Wally",
+            b: "Chickaletta",
+            c: "Ryder"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What name is given to the puppy who wears a blue police unifrom?",
+        answers: {
+            a: "Chase",
+            b: "Rumble",
+            c: "Tracker"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is?",
+        answers: {
+            a: "Major Goodway",
+            b: "Major Humdinger",
+            c: "Major Johnson"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What name is given to the puppy who wears a pink helicopter pilot uniform?",
+        answers: {
+            a: "Rocky",
+            b: "Everest",
+            c: "Sky"
+        },
+        correctAnswer: "a"
+    },
+];
+
+function quizGame(gameLevel) {
+
+}
+
+function findOutAnswer() {
+
+}
+
+function correctAnswerScore() {
+
+}
+
+function incorrectAnswerScore() {
+
+}
