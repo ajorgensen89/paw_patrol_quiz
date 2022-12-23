8b7b5683444bb22fab63fa48e6fc05ad9e5759b4
ajorgensen89 <ajorgensen89@gmail.com>
2022-12-23 15:23:29 +0000

    add line 19-20 to iterrate my questions ut not functioning correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,31 @@
+var questionId = 0;
+
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function (event) {
             if (this.getAttribute("data-type") === "submit") {
-                console.log("yes")
-                iterate(id);
-            } else {
+                event.preventDefault();
+                console.log("yes");
+                iterate(questionId);
+            }
+            else {
                 let  = this.getAttribute("data-type");
+                iterate(questionId);
                 console.log("yesagain")
                     ();
             }
+            iterate(questionId);
+            console.log("ok");
         });
     }
 });
 /**setting question array */
+
 let numberOfQuestions = [{
         id: 0,
-        question: "What is naughty Majors name in Paw Patrol who cases problems for the Paw Patrol gang?",
+        question: "What is naughty Majors name in Paw Patrol who causes problems for the Paw Patrol gang?",
         answers: [{
                 choice: "Major Goodway",
                 isCorrect: false
@@ -60,7 +68,7 @@ let numberOfQuestions = [{
             },
             {
                 choice: "Chickaletta",
-                iscorrect: true
+                isCorrect: true
             },
             {
                 choice: "Ryder",
@@ -98,29 +106,35 @@ let numberOfQuestions = [{
     // },
 ];
 /** setting start of quiz to run */
-let start = true;
+// let start = true;
 /** setting function to run quiz via iterating the ID for my questions. */
 function iterate(id) {
     /** getting the window to show the result by class in div*/
+    console.log("1");
     let outcome = document.getElementsByClassName("outcome");
-    outcome[0].innerText = "";
+    outcome[0].textContent = "";
     /** getting question from array*/
     let question = document.getElementById("question");
     /**getting inner text of the question in my array */
-    question.innerText = numberOfQuestions[id].question;
+                                                         /**NEED HELP WITH THIS LINE 124 AS CAN NOT CREATE LOOP CORRECTLY KEEPS MAKING IT INFINITE.
+                                                          * If wont iterate correctly through my questions.
+                                                          */
+    question.textContent = numberOfQuestions[id].question;
+    
     /**Getting my 3 different answer choice boxs from html */
     let answer1 = document.getElementById("answer1");
     let answer2 = document.getElementById("answer2");
     let answer3 = document.getElementById("answer3");
+    console.log("2");
     /** getting innertext of my answer choices from my answer arrays */
-    answer1.innerText = numberOfQuestions[id].answers[0].choice;
-    answer2.innerText = numberOfQuestions[id].answers[1].choice;
-    answer3.innerText = numberOfQuestions[id].answers[2].choice;
+    answer1.textContent = numberOfQuestions[id].answers[0].choice;
+    answer2.textContent = numberOfQuestions[id].answers[1].choice;
+    answer3.textContent = numberOfQuestions[id].answers[2].choice;
     /** idenifying the correct option from the answer in the array */
     answer1.value = numberOfQuestions[id].answers[0].isCorrect;
     answer2.value = numberOfQuestions[id].answers[1].isCorrect;
     answer3.value = numberOfQuestions[id].answers[2].isCorrect;
-
+    console.log("3");
     /** Add event listeners to the 3 answers */
     let confirmed = "";
     answer1.addEventListener("click", () => {
@@ -141,33 +155,34 @@ function iterate(id) {
         answer3.style.backgroundColor = "green";
         confirmed = answer3.value;
     });
-/**get html element check gtom html */
+    console.log("4");
+    /**get html element check from html */
     let check = document.getElementsByClassName("check");
-/**comfirm the view of correct or incorrect choices for user */
+
+    /**comfirm the view of correct or incorrect choices for user */
     check[0].addEventListener("click", () => {
-        if (confirmed === "true") {
-            outcome[0].innerHTML = "True";
-            outcome[0].style.color = "green";
-        } else {
-            outcome[0].innerHTML = "False";
-            outcome[0].style.color = "red";
-        }
-    })
+         if (confirmed === "green") {
+            isCorrect = true;
+             outcome[0].textContent = "True";
+             outcome[0].style.color = "green";
+         } else (confirmed === "red"); {
+            isCorrect = false;
+             outcome[0].textContent = "False";
+             outcome[0].style.color = "red";
+         }
+     })
+    console.log("5");
     /**start the quiz */
-    if (start) {
-        iterate("0");
-    }
+    /*    if (start) {
+            console.log("6");
+            iterate("0");
+        }*/
 
-    /**next question button */ 
+    /**next question button */
     let next = document.getElementsByClassName("next")[0];
-    let id = 0;
-
+    console.log("7");
     next.addEventListener("click", () => {
-        start = false;
-        if (id < 2) {
-            id++;
-            iterate(id);
-            console.log(id);
-        }
+        questionId++;
+        iterate(questionId);
     });
 }
